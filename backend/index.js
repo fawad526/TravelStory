fcc3f211a7ca9e91d623782677e516662a651a24
fawadiqbal <fawdiqbal274@gmail.com>
2024-09-26 21:19:03 +0500

    new commit

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,7 @@ const __dirname = path.dirname(__filename);
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_DB_URL)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
