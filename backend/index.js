d4da22eb41132ddfa385b31d9225aa99a362f915
fawadiqbal <fawdiqbal274@gmail.com>
2024-09-27 01:13:06 +0500

    new commit

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://travelstory-frontend.vercel.app",
+    origin: "https://travel-story-o18hzltfn-fawad526s-projects.vercel.app/",
     credentials: true,
   })
 );
