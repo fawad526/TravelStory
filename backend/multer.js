501345c53603191110e8e58c1e4b16cdc4434d67
fawadiqbal <fawdiqbal274@gmail.com>
2024-09-27 22:19:09 +0500

    new commit

--- backend/multer.js
+++ backend/multer.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "./cloudinaryConfig";
+import cloudinary from "./cloudinaryConfig.js";
 
 // Configure Cloudinary
 cloudinary.config({
